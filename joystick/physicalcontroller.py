83d14e9b31fdcb4d4c1c9347814e6a76b096e370
Prateek Sahay <ptkinvent@gmail.com>
2020-02-04 22:48:56 -0500

    Renamed extern/ and thirdParty/ to lib/ for consistency

--- joystick/physicalcontroller.py
+++ joystick/physicalcontroller.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020 FRC Team 3260
 #
 
-from thirdParty.frc4564_xbox_python.frc4564_xbox_python import Joystick
+from lib.frc4564_xbox_python.xbox import Joystick
 from comms import *
 from threading import Thread
 
